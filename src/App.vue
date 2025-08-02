febf7c358aff1ee458d57a22fa1409c8a66ad886
clarencelinwork <clarencelinwork@gmail.com>
2025-08-03 01:38:15 +0800

    [feature] LV3 Homework

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,98 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue';
+
+const items = ref([
+  {
+    "id": 1,
+    "name": "珍珠奶茶",
+    "description": "香濃奶茶搭配QQ珍珠",
+    "price": 50,
+    "stock": 20,
+  }, {
+    "id": 2,
+    "name": "冬瓜檸檬",
+    "description": "清新冬瓜配上新鮮檸檬",
+    "price": 45,
+    "stock": 18,
+  }, {
+    "id": 3,
+    "name": "翡翠檸檬",
+    "description": "綠茶與檸檬的完美結合",
+    "price": 55,
+    "stock": 34,
+  }, {
+    "id": 4,
+    "name": "四季春茶",
+    "description": "香醇四季春茶，回甘無比",
+    "price": 45,
+    "stock": 10,
+  }, {
+    "id": 5,
+    "name": "阿薩姆奶茶",
+    "description": "阿薩姆紅茶搭配香醇鮮奶",
+    "price": 50,
+    "stock": 25,
+  }, {
+    "id": 6,
+    "name": "檸檬冰茶",
+    "description": "檸檬與冰茶的清新組合",
+    "price": 45,
+    "stock": 20,
+  }, {
+    "id": 7,
+    "name": "芒果綠茶",
+    "description": "芒果與綠茶的獨特風味",
+    "price": 55,
+    "stock": 18,
+  }, {
+    "id": 8,
+    "name": "抹茶拿鐵",
+    "description": "抹茶與鮮奶的絕配",
+    "price": 60,
+    "stock": 20,
+  },
+
+]);
+
+function plus(id){
+  let item = items.value.find(item => item.id === id);
+  item.stock++
+}
+
+function minus(id){
+  let item = items.value.find(item => item.id === id);
+  item.stock--
+
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <table border="1" style="width: 90%;margin: 50px;">
+    <thead>
+      <tr>
+        <th scope="col">品項</th>
+        <th scope="col">描述</th>
+        <th scope="col">價格</th>
+        <th scope="col">庫存</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in items" :key="item.id">
+        <td>
+          <input type="text" v-model="item.name">
+        </td>
+        <td>{{ item.description }}</td>
+        <td>{{ item.price }}</td>
+        <td>
+          <button v-on:click="minus(item.id)">-</button>
+          <input type="number" v-model="item.stock">
+          <button v-on:click="plus(item.id)">+</button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
